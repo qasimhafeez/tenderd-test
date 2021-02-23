673e65a3a224e2fb23178d869fabc8b596101534
Qasim Hafeez <qasimh556@gmail.com>
2021-02-23 12:23:15 +0400

    signUp Page

---
+++ client/src/components/auth/signUp.js
@@ -0,0 +1,174 @@
+// Auth Context
+import { useAuth } from "../../contexts/AuthContext";
+// React Hooks
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+// React Router
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+// Material Design UI
+import {
+  TextField,
+  Grid,
+  Container,
+  Button,
+  makeStyles,
+  Paper,
+} from "@material-ui/core";
+import Alert from "@material-ui/lab/Alert";
+
+// Custom Styles
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  },
+  paper: {
+    width: "100%",
+    padding: "2rem",
+  },
+  paperFormDiv: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  textFieldMargin: {
+    margin: "1.2rem 0 0 0",
+  },
+  lastSection: {
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    marginTop: "1rem",
+  },
+  errorPTag: {
+    fontSize: ".9rem",
+    color: "red",
+    marginTop: "-.1rem",
+  },
+  buttonStyle: {
+    marginTop: "1.2rem",
+  },
+  headingH1: {
+    textAlign: "center",
+    color: "#3f51b5",
+  },
+  linkStyle: {
+    color: "#3f51b5",
+  },
+  customErrors: {
+    fontSize: "1rem",
+    color: "red",
+  },
+});
+
+const SignUp = () => {
+  const { register, errors, handleSubmit } = useForm();
+
+  // Initialize classes
+  const classes = useStyles();
+
+  // Local state
+  const [err, setErr] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const { push } = useHistory();
+  const { signUp } = useAuth();
+
+  const handleFormSubmit = (data) => {
+    if (data.password !== data.confirm_password) {
+      setErr("Passwords did not match");
+    } else {
+      setErr("");
+      setLoading(true);
+      signUp(data.email, data.password)
+        .then(() => {
+          setLoading(false);
+          push("/signin");
+        })
+        .catch(() => {
+          setLoading(false);
+          setErr("Email already exists");
+        });
+    }
+  };
+
+  return (
+    <Container maxWidth="xs">
+      <Grid container className={classes.root}>
+        <Paper className={classes.paper} elevation={7}>
+          <h1 className={classes.headingH1}>Sign Up</h1>
+          {err && <Alert severity="error">{err}</Alert>}
+          <form
+            className={classes.paperFormDiv}
+            onSubmit={handleSubmit(handleFormSubmit)}
+          >
+            <TextField
+              fullWidth
+              label="Email"
+              className={classes.textFieldMargin}
+              name="email"
+              variant="outlined"
+              inputRef={register({
+                required: true,
+                pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+              })}
+            />
+
+            {errors.email?.type === "pattern" && (
+              <p className={classes.errorPTag}>
+                please enter correct email address
+              </p>
+            )}
+            <TextField
+              fullWidth
+              label="Password"
+              className={classes.textFieldMargin}
+              name="password"
+              variant="outlined"
+              type="password"
+              inputRef={register({ required: true, minLength: 5 })}
+            />
+            {errors.password?.type === "minLength" && (
+              <p className={classes.errorPTag}>minimum length 5*</p>
+            )}
+
+            <TextField
+              className={classes.textFieldMargin}
+              name="confirm_password"
+              label="Confirm Password"
+              variant="outlined"
+              type="password"
+              inputRef={register({ required: true, minLength: 5 })}
+            />
+            {errors.confirm_password?.type === "minLength" && (
+              <p className={classes.errorPTag}>minimum length 5*</p>
+            )}
+
+            <Button
+              disabled={loading}
+              type="submit"
+              color="primary"
+              variant="contained"
+              size="large"
+              className={classes.buttonStyle}
+            >
+              {loading ? "Signing Up..." : "Sign Up"}
+            </Button>
+          </form>
+          <section className={classes.lastSection}>
+            <p>
+              Already have an account?{" "}
+              <Link className={classes.linkStyle} to="/signin">
+                Sign In
+              </Link>
+            </p>
+          </section>
+        </Paper>
+      </Grid>
+    </Container>
+  );
+};
+
+export default SignUp;
