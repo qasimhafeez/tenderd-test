b0119524af7f77e4ea3a00b23144f6d1e0a80cd9
Qasim Hafeez <qasimh556@gmail.com>
2021-02-23 12:54:01 +0400

    protected Routes

---
+++ client/src/components/common/userProtectedRoute.js
@@ -0,0 +1,29 @@
+import { Redirect, Route } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
+
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const { currentUser } = useAuth();
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (!currentUser) {
+          return <Component {...props} />;
+        } else {
+          return (
+            <Redirect
+              to={{
+                pathname: "/signin",
+                state: {
+                  from: props.location,
+                },
+              }}
+            />
+          );
+        }
+      }}
+    />
+  );
+};
+
+export default ProtectedRoute;
