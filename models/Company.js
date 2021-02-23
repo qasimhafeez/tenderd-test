00115fba729d1f760b24ebea8b197f48945799fb
Qasim Hafeez <qasimh556@gmail.com>
2021-02-23 16:50:17 +0400

    Company Model

---
+++ models/Company.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CompanySchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    ref: "users",
+    type: Schema.Types.ObjectId,
+  },
+});
+
+module.exports = Company = mongoose.model("company", CompanySchema);
