d29253da25eeaf0fc031b94a38434f1732df5c2d
Qasim Hafeez <qasimh556@gmail.com>
2021-02-23 11:55:59 +0400

    setting up routes and context-api

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,37 @@
-import "./App.css";
+// Context Api
+import ContextsIndex from "./contexts/index";
+
+// React Router
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+// Importing Components
+import SignUp from "./components/auth/signUp";
+import SignIn from "./components/auth/signIn";
+import ForgotPassword from "./components/auth/forgetPassword";
+import Profile from "./components/auth/userProfile";
+import ProtectedRoute from "./components/common/protectedRoute";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <ContextsIndex>
+        <Router>
+          <Switch>
+            <Redirect exact from="/" to="/signup" />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/signin" component={SignIn} />
+            <Route exact path="/forgotpassword" component={ForgotPassword} />
+            <ProtectedRoute exact path="/userprofile" component={Profile} />
+          </Switch>
+        </Router>
+      </ContextsIndex>
+    </div>
+  );
 }
 
 export default App;
