fa89e71361717341b85f77be54499307b073f715
Qasim Hafeez <qasimh556@gmail.com>
2021-02-24 17:47:46 +0400

    Request Api --under-development

--- routes/api/request.js
+++ routes/api/request.js
@@ -6,4 +6,35 @@ const Request = require("../../models/Request");
 const User = require("../../models/User");
 const Company = require("../../models/Company");
 
+// @route   Post api/request/create
+// @desc    Create a new request
+// @access  Public
+router.post("/create", async (req, res) => {
+  // Destruturing body
+  const {
+    type,
+    description,
+    image,
+    status,
+    created_req_time,
+    companyId,
+    userId,
+    history,
+  } = req.body;
+
+  const newRequest = new Request({
+    type,
+    description,
+    image,
+    status,
+    created_req_time,
+    companyId,
+    userId,
+    history,
+  });
+
+  await newRequest.save();
+  res.send(newRequest);
+});
+
 module.exports = router;
