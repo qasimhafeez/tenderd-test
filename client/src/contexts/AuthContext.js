b205ffae68955b073ed0fba13aa8d197f295a0c9
Qasim Hafeez <qasimh556@gmail.com>
2021-02-24 20:15:40 +0400

    added functionality to save users in db

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,57 +1,67 @@
-import { createContext, useState, useEffect, useContext } from "react";
-
+import { createContext, useState, useEffect, useContext } from 'react'
+import axios from 'axios'
 //firebase auth
-import { auth } from "../firebase/config";
+import { auth } from '../firebase/config'
 
-export const AuthContext = createContext();
+export const AuthContext = createContext()
 
 //usecontext hook
 export const useAuth = () => {
-  return useContext(AuthContext);
-};
+	return useContext(AuthContext)
+}
 
 export const AuthProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [loading, setLoading] = useState(true);
+	const [currentUser, setCurrentUser] = useState(null)
+	const [userData, setUserData] = useState(null)
+	const [isAuthenticated, setIsAuthenticated] = useState(false)
+	const [loading, setLoading] = useState(true)
 
-  //signup function
-  const signUp = (email, password) => {
-    return auth.createUserWithEmailAndPassword(email, password);
-  };
+	//signup function
+	const signUp = (email, password, name) => {
+		return auth.createUserWithEmailAndPassword(email, password).then(user => {
+			const userData = {
+				email: user.user.email,
+				name: name,
+			}
+			axios
+				.post('http://localhost:5000/api/user/create', userData)
+				.then(res => console.log({ res }))
+		})
+	}
 
-  //login / signin function
-  const signIn = (email, password) => {
-    return auth.signInWithEmailAndPassword(email, password);
-  };
+	//login / signin function
+	const signIn = (email, password) => {
+		return auth.signInWithEmailAndPassword(email, password)
+	}
 
-  //logout function
-  const logout = (email, password) => {
-    return auth.signOut();
-  };
+	//logout function
+	const logout = () => {
+		return auth.signOut()
+	}
 
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      setCurrentUser(user);
-      setLoading(false);
-    });
+	useEffect(() => {
+		const unsubscribe = auth.onAuthStateChanged(user => {
+			setCurrentUser(user)
+			setLoading(false)
+		})
 
-    localStorage.getItem("isAuthenticated");
+		localStorage.getItem('isAuthenticated')
 
-    return unsubscribe;
-  }, []);
+		return unsubscribe
+	}, [])
 
-  const value = {
-    currentUser,
-    signUp,
-    signIn,
-    isAuthenticated,
-    logout,
-  };
+	const value = {
+		currentUser,
+		signUp,
+		signIn,
+		isAuthenticated,
+		logout,
+		userData,
+	}
 
-  return (
-    <AuthContext.Provider value={value}>
-      {!loading && children}
-    </AuthContext.Provider>
-  );
-};
+	return (
+		<AuthContext.Provider value={value}>
+			{!loading && children}
+		</AuthContext.Provider>
+	)
+}
