0908bf4229a88a09b7a80f173e1de5ec5977ab59
Qasim Hafeez <qasimh556@gmail.com>
2021-02-24 11:52:46 +0400

    mongodb URI (testing purpose)

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+// This is Test so that's why it is not saved in .env file
+
+module.exports = {
+  mongoURI:
+    "mongodb+srv://admin:admin@testcluster.jquvd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  secretOrKey: "secret",
+};
