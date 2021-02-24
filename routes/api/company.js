605546c153d70374e71aa978359e558bb0d85884
Qasim Hafeez <qasimh556@gmail.com>
2021-02-24 11:51:55 +0400

    api for registering company and query all companies

--- routes/api/company.js
+++ routes/api/company.js
@@ -5,4 +5,30 @@ const router = express.Router();
 const Company = require("../../models/Company");
 const User = require("../../models/User");
 
+// @route   POST api/company/add
+// @desc    Add a new company
+// @access  Public
+
+router.post("/add", async (req, res) => {
+  let err;
+  const newCompany = new Company({
+    name: req.body.company,
+  });
+  await newCompany.save();
+  res.send(newCompany);
+});
+
+// @route   GET api/company/list
+// @desc    complete list of companies
+// @access  Public
+
+router.get("/list", async (req, res) => {
+  const err = "No Company is registered yet!";
+  const companies_list = await Company.find();
+  if (!companies_list) {
+    res.send({ err });
+  }
+  res.status(200).send({ companies_list });
+});
+
 module.exports = router;
