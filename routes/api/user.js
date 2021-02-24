74e11251287564d87007cd7b6656c066f8a2f3ee
Qasim Hafeez <qasimh556@gmail.com>
2021-02-24 12:48:44 +0400

    user api-testing

--- routes/api/user.js
+++ routes/api/user.js
@@ -4,4 +4,30 @@ const router = express.Router();
 // Loading Models
 const User = require("../../models/User");
 
+// @route   Post api/user/create
+// @desc    Add a new user
+// @access  Public
+router.post("/create", async (req, res) => {
+  const { name, email } = req.body;
+  const newUser = new User({
+    name,
+    email,
+  });
+
+  await newUser.save();
+  res.status(201).send(newUser);
+});
+
+// @route   GET api/user/all
+// @desc    Show complete list of users (Testing Purpose)
+// @access  Public
+router.post("/all", async (req, res) => {
+  const users_list = await User.find();
+  if (!users_list) {
+    res.status(404).send({ error: "No User Found!" });
+  }
+
+  res.status(201).send(users_list);
+});
+
 module.exports = router;
